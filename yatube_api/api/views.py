7fae917f5d36f0164639154fa4127d5877382f37
Максим <maksim@MacBook-Air-Exon.local>
2022-11-09 02:07:36 +0300

    Первое ревью

---
+++ yatube_api/api/views.py
@@ -0,0 +1,36 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from django.shortcuts import get_object_or_404
+
+from .permissions import IsAuthorOrReadOnly
+from posts.models import Group, Post
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs['post_id'])
+
+    def get_queryset(self):
+        return self.get_post().comments
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post=self.get_post())
